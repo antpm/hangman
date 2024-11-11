95c6ce8faf90beac38cc580174b400393285e567
Anthony <anthonypm.29590@gmail.com>
2024-11-11 14:27:55 -0400

    guessing and win/lose logic implemented

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,6 +1,10 @@
 class Hangman
   def initialize(word)
     @word = word
+    @guesses = 0
+    @win = false
+    @hidden_word = @word.gsub(/[a-z A-Z]/, "_")
+
   end
 
   def show_word
@@ -9,6 +13,42 @@ class Hangman
 
   def start_game
     puts "It's time to play"
-    show_word
+    until @guesses >= 5 or @win
+      puts "Your word:"
+      puts @word
+      puts @hidden_word
+      puts "Incorrect Guesses: #{@guesses}"
+      puts "Guess a letter: "
+      guess = gets.chomp
+      if check_guess(guess)
+        puts "Correct"
+        if @hidden_word == @word
+          @win = true
+        end
+      else 
+        puts "Incorrect"
+        @guesses += 1
+      end
+    end
+
+    if @win
+      puts "You won!"
+    else 
+      puts "You lost..."
+    end
+    
+  end
+
+  def check_guess(guess)
+    if @word.include?(guess)
+      @word.each_char.with_index do |letter, i|
+        if guess == letter
+        @hidden_word[i] = guess
+        end
+      end
+      return true
+    else
+      return false
+    end
   end
 end
